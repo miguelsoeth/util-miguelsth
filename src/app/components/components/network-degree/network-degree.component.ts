d78ed930111f26a912cf5e250275028021f3cbc8
Miguel Soeth <miguelsoeth@gmail.com>
2024-10-11 02:15:52 -0300

    FIXED IMAGES!

--- src/app/components/components/network-degree/network-degree.component.ts
+++ src/app/components/components/network-degree/network-degree.component.ts
@@ -6,6 +6,7 @@ import { SearchFieldComponent } from '../../template/search-field/search-field.c
 import { NativeSearchFieldComponent } from '../../template/native-search-field/native-search-field.component';
 import { NetworkDegreeService } from '../../../services/network-degree.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-network-degree',
@@ -19,14 +20,13 @@ export class NetworkDegreeComponent implements OnInit {
   header = inject(HeaderService);
   network = inject(NetworkDegreeService);
   route = inject(ActivatedRoute);
-  router = inject(Router)
+  router = inject(Router);
+  http = inject(HttpClient);
 
   originActor: string = '';
   destinyActor: string = '';
   result?: string[];
-
-  moviesList: string[] = [];
-  actorsList: string[] = [];
+  images: { [key: string]: string } = {};
 
   ngOnInit(): void {
     this.header.setTitle("Network degree");
@@ -36,6 +36,34 @@ export class NetworkDegreeComponent implements OnInit {
       this.destinyActor = params['destiny'] || '';
 
       this.calculateNetworkDegree();
+    });
+  }
+
+  calculateNetworkDegree() {
+    if (this.originActor && this.destinyActor) {
+      this.network.getNetwork(this.originActor, this.destinyActor).subscribe({
+        next: (result): void => {
+          this.result = result;
+          this.loadImages(result);
+        },
+        error: (err) => console.error('Error calculating network degree:', err)
+      })
+    }    
+  }
+
+  loadImages(result: string[]) {
+    result.forEach((item) => {
+      // Fetch image URL from your API for each item
+      this.http.get<string[]>(`http://localhost:3000/api/image/${item}`).subscribe({
+        next: (imageUrls) => {
+          if (imageUrls.length > 0) {
+            this.images[item] = `https://${imageUrls[0]}`;
+          } else {
+            console.warn(`No images found for item: ${item}`);
+          }
+        },
+        error: (err) => console.error('Error fetching image URLs:', err)
+      });
     });
   }
 
@@ -57,36 +85,6 @@ export class NetworkDegreeComponent implements OnInit {
       };
       this.router.navigate(['/network-degree'], { queryParams }); // Navigates to the URL with query parameters
     }
-  }
-
-  calculateNetworkDegree() {
-    if (this.originActor && this.destinyActor) {
-      this.network.getNetwork(this.originActor, this.destinyActor).subscribe({
-        next: (result): void => {
-          console.log(result);
-          this.setResult(result);
-          this.result = result;
-        },
-        error: (err) => console.error('Error calculating network degree:', err)
-      })
-    }    
-  }
-
-  setResult(result: string[]) {
-    this.actorsList = [];
-    this.moviesList = [];
-
-    result.forEach((item, index) => {
-      if (index%2==0) {
-        this.actorsList.push(item);
-      }
-      else {
-        this.moviesList.push(item);
-      }
-    });
-
-    console.log(this.actorsList);
-    console.log(this.moviesList);
   }
 
 }
